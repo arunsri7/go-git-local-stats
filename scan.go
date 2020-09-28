012cee6f43d7a1a9cdc8c87ecb7c18463724c685
arunsri7 <arun.i1997@gmail.com>
2020-09-28 21:47:33 +0530

    implemeneted recursive scanning of .git folders

---
+++ scan.go
@@ -0,0 +1,149 @@
+package main
+
+//check why we call recursive using another function
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/user"
+	"strings"
+)
+
+func main() {
+	scan("/home/aruniyer/Projects/golang")
+}
+
+//function to handle the scanning, and writing the list of repos to a file
+func scan(folder string) {
+	repositories := scanGitFolders(make([]string, 0), folder)
+	filePath := getDotFilePath()
+	addNewSliceElementsToFile(filePath, repositories)
+}
+
+// func recursiveScanFolder(folder string) []string {
+// 	return scanGitFolders(make([]string, 0), folder)
+// }
+
+// scanGitFolders returns a list of subfolders of `folder` ending with `.git`.
+// Returns the base folder of the repo, the .git folder parent.
+// Recursively searches in the subfolders by passing an existing `folders` slice.
+func scanGitFolders(folders []string, folder string) []string {
+	folder = strings.TrimSuffix(folder, "/")
+	f, err := os.Open(folder)
+	if err != nil {
+		log.Fatal(err)
+	}
+	files, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var path string
+	for _, file := range files {
+		if file.IsDir() {
+			path = folder + "/" + file.Name()
+			if file.Name() == ".git" {
+				path = strings.TrimSuffix(path, "/.git")
+				fmt.Println(path)
+				folders = append(folders, path)
+				continue
+			}
+			if file.Name() == "vendor" || file.Name() == "node_modules" {
+				continue
+			}
+			folders = scanGitFolders(folders, path)
+		}
+	}
+	return folders
+}
+
+// creates a dot file named "gogitlocalstats" in the home directory
+func getDotFilePath() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dotFile := usr.HomeDir + "/.gogitlocalstats"
+	return dotFile
+
+}
+
+// parse the existing repos stored in the file to a slice
+// add the new items to the slice, without adding duplicates
+// store the slice to the file, overwriting the existing content
+
+func addNewSliceElementsToFile(filePath string, newRepos []string) {
+	existingRepos := parseFileLinesToSlice(filePath)
+	repos := joinSlices(newRepos, existingRepos)
+	dumpStringsSliceToFile(repos, filePath)
+}
+
+// parseFileLinesToSlice given a file path string, gets the content
+// of each line and parses it to a slice of strings.
+func parseFileLinesToSlice(filePath string) []string {
+	f := openFile(filePath)
+	defer f.Close() //?
+	var lines []string
+	scanner := bufio.NewScanner(f) //?
+	for scanner.Scan() {           //?
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		print(lines)
+		if err != io.EOF {
+			panic(err)
+		}
+		panic(err)
+	}
+	print(lines)
+	return lines
+}
+
+// openFile opens the file located at `filePath`. Creates it if not existing.
+func openFile(filePath string) *os.File {
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0755)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// file does not exist
+			_, err = os.Create(filePath)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			panic(err)
+		}
+	}
+
+	return f
+}
+
+// joinSlices adds the element of the `new` slice
+// into the `existing` slice, only if not already there
+func joinSlices(new []string, existing []string) []string {
+	for _, i := range new {
+		if !sliceContains(existing, i) {
+			existing = append(existing, i)
+		}
+	}
+	return existing
+}
+
+// sliceContains returns true if `slice` contains `value`
+func sliceContains(slice []string, value string) bool {
+	for _, v := range slice {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
+
+// dumpStringsSliceToFile writes content to the file in path `filePath` (overwriting existing content)
+func dumpStringsSliceToFile(repos []string, filePath string) {
+	content := strings.Join(repos, "\n")
+	ioutil.WriteFile(filePath, []byte(content), 0755)
+}
