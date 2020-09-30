e456c59e25c7342d0a38be093fd6f626170d301a
arunsri7 <arun.i1997@gmail.com>
2020-09-30 22:43:07 +0530

    [Added stats.go and go mod file]

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"go-git-graph/scan"
+	"go-git-graph/stats"
+)
+
+func main() {
+	var folder string
+	var email string
+	flag.StringVar(&folder, "add", "", "add a new folder to scan for Git repositories")
+	flag.StringVar(&email, "email", "your@email.com", "the email to scan")
+	flag.Parse()
+
+	if folder != "" {
+		scan.Scan(folder)
+		return
+	}
+
+	stats.Stats(email)
+}
--- scan/scan.go
+++ scan/scan.go
@@ -1,9 +1,8 @@
-package main
+package scan
 
 //TODO
-//check why we call recursive using another function
 //check EOF & Scanner in  error in parseFileLinesToSlice
-
+//Add appropriate comments
 import (
 	"bufio"
 	"fmt"
@@ -14,20 +13,12 @@ import (
 	"strings"
 )
 
-func main() {
-	scan("/home/aruniyer/Projects")
-}
-
-//function to handle the scanning, and writing the list of repos to a file
-func scan(folder string) {
+func Scan(folder string) {
 	repositories := scanGitFolders(make([]string, 0), folder)
-	filePath := getDotFilePath()
+	filePath := GetDotFilePath()
 	addNewSliceElementsToFile(filePath, repositories)
 }
 
-// scanGitFolders returns a list of subfolders of `folder` ending with `.git`.
-// Returns the base folder of the repo, the .git folder parent.
-// Recursively searches in the subfolders by passing an existing `folders` slice.
 func scanGitFolders(folders []string, folder string) []string {
 	folder = strings.TrimSuffix(folder, "/")
 	f, err := os.Open(folder)
@@ -59,9 +50,7 @@ func scanGitFolders(folders []string, folder string) []string {
 	return folders
 }
 
-// creates a dot file named "gogitlocalstats" in the
-//home directory
-func getDotFilePath() string {
+func GetDotFilePath() string {
 	usr, err := user.Current()
 	if err != nil {
 		log.Fatal(err)
@@ -71,19 +60,13 @@ func getDotFilePath() string {
 
 }
 
-// parse the existing repos stored in the file to a slice
-// add the new items to the slice, without adding duplicates
-// store the slice to the file, overwriting the existing content
-
 func addNewSliceElementsToFile(filePath string, newRepos []string) {
-	existingRepos := parseFileLinesToSlice(filePath)
+	existingRepos := ParseFileLinesToSlice(filePath)
 	repos := joinSlices(newRepos, existingRepos)
 	dumpStringsSliceToFile(repos, filePath)
 }
 
-// parseFileLinesToSlice given a file path string, gets the content
-// of each line and parses it to a slice of strings.
-func parseFileLinesToSlice(filePath string) []string {
+func ParseFileLinesToSlice(filePath string) []string {
 	f := openFile(filePath)
 	defer f.Close()
 	var lines []string
@@ -92,19 +75,15 @@ func parseFileLinesToSlice(filePath string) []string {
 		lines = append(lines, scanner.Text())
 	}
 	if err := scanner.Err(); err != nil {
-		print(lines)
 		// if err != io.EOF {
 		// 	panic(err)
 		// }
 	}
-	print(lines)
 	return lines
 }
 
-// openFile opens the file located at `filePath`.
-//Creates it if not existing.
 func openFile(filePath string) *os.File {
-	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0755)
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_RDWR, 0755)
 	if err != nil {
 		if os.IsNotExist(err) {
 			// If file does not exist
@@ -120,8 +99,6 @@ func openFile(filePath string) *os.File {
 	return f
 }
 
-// joinSlices adds the element of the `new` slice
-// into the `existing` slice, only if not already there
 func joinSlices(new []string, existing []string) []string {
 	for _, i := range new {
 		if !sliceContains(existing, i) {
@@ -131,7 +108,6 @@ func joinSlices(new []string, existing []string) []string {
 	return existing
 }
 
-// sliceContains returns true if `slice` contains `value`
 func sliceContains(slice []string, value string) bool {
 	for _, v := range slice {
 		if v == value {
@@ -141,7 +117,6 @@ func sliceContains(slice []string, value string) bool {
 	return false
 }
 
-// dumpStringsSliceToFile writes content to the file in path `filePath` (overwriting existing content)
 func dumpStringsSliceToFile(repos []string, filePath string) {
 	content := strings.Join(repos, "\n")
 	ioutil.WriteFile(filePath, []byte(content), 0755)
